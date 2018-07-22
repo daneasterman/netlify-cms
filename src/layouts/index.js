a0b5df92c8fe86cdf0e289a74a5a372ef1cd8c6b
Daniel Easterman <daniel.easterman@gmail.com>
2018-07-22 17:36:59 +0100

    Timeline is now displaying on page navigation , not manual reload, but height + width properties are now still inconsistently loading

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 // import Script from "react-load-script";
@@ -8,58 +8,17 @@ import Navbar from "../components/Navbar";
 import "./all.sass";
 
 class TemplateWrapper extends React.Component {
-
-  constructor(props) {
-    super(props);
-    // create a ref to store the textInput DOM element
-    this.timeline = React.createRef();
-    // this.focusTextInput = this.focusTextInput.bind(this);
-  }
-
-  componentDidMount() {
-    // window.initTimeline = this.initTimeline;
-
-    loadJS('https://cdn.knightlab.com/libs/timeline3/latest/js/timeline.js');
-
-    new TL.Timeline(this.refs.timeline, "../data/england.json", {
-      // ga_property_id: "UA-XXXX" 
-    });
-    // window.onresize = function(event) {
-    //   timeline.updateDisplay();
-    // };
-  }
-
-  // initTimeline() {
-  //   const timeline = new TL.Timeline(this.refs.timeline, "../data/england.json", {
-  //     // ga_property_id: "UA-XXXX" 
-  //   });
-  //   window.onresize = function(event) {
-  //     timeline.updateDisplay();
-  //   };
-  // }
-
-  // All this file should be in separate component inside blog post. TRY THAT OUT NEXT.
-
   render() {
     const { children } = this.props;
 
     return (
       <div>
         <Helmet title="Daniel Easterman" />
-        <Navbar />        
+        <Navbar />
         <div>{children()}</div>
-        <div ref="timeline" style={{height: '100%', width: '100%'}}></div>
       </div>
     );
-  }  
-}
-
-function loadJS(src) {
-  var ref = window.document.getElementsByTagName("script")[0];
-  var script = window.document.createElement("script");
-  script.src = src;
-  script.async = true;
-  ref.parentNode.insertBefore(script, ref);
+  }
 }
 
 TemplateWrapper.propTypes = {
@@ -77,12 +36,12 @@ export default TemplateWrapper;
 // );
 
 // executeAllBlogTimelines() {
-  //   let scripts = document.querySelectorAll(
-  //     '[data-inline-script="data-inline-script"]'
-  //   );
-  //   scripts.forEach(function forEachScript(element) {
-  //     const script = element.innerHTML;
-  //     window.Function(script)();
-  //     // window.eval(script);
-  //   });
-  // }
+//   let scripts = document.querySelectorAll(
+//     '[data-inline-script="data-inline-script"]'
+//   );
+//   scripts.forEach(function forEachScript(element) {
+//     const script = element.innerHTML;
+//     window.Function(script)();
+//     // window.eval(script);
+//   });
+// }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,54 +4,87 @@ import { kebabCase } from "lodash";
 import Helmet from "react-helmet";
 import Link from "gatsby-link";
 import Content, { HTMLContent } from "../components/Content";
+import Script from "react-load-script";
 
-export const BlogPostTemplate = ({
-  content,
-  contentComponent,
-  description,
-  tags,
-  title,
-  helmet
-}) => {
-  const PostContent = contentComponent || Content;
+// Try this after react-load-script tag solution.
 
-  return (
-    <section className="section">
-      <Helmet>
-        <link
-          title="timeline-styles"
-          rel="stylesheet"
-          href="https://cdn.knightlab.com/libs/timeline3/latest/css/timeline.css"
+// function loadJS(src) {
+//   var ref = window.document.getElementsByTagName("script")[0];
+//   var script = window.document.createElement("script");
+//   script.src = src;
+//   script.async = true;
+//   ref.parentNode.insertBefore(script, ref);
+// }
+
+class BlogPostTemplate extends React.Component {
+  constructor(props) {
+    super(props);    
+  }
+
+  displayTimeline() {
+    const timelineID = document.getElementById('timelineID');
+
+    const timeline = new TL.Timeline(timelineID, "../../data/england.json");
+    window.onresize = function(event) {
+      timeline.updateDisplay();
+    };
+
+  }
+
+  render() {
+    const {
+      content,
+      contentComponent,
+      description,
+      tags,
+      title,
+      helmet
+    } = this.props;
+
+    const PostContent = contentComponent || Content;
+
+    return (
+      <section className="section">
+        <Helmet>
+          <link
+            title="timeline-styles"
+            rel="stylesheet"
+            href="https://cdn.knightlab.com/libs/timeline3/latest/css/timeline.css"
+          />
+        </Helmet>
+        <Script
+          url="https://cdn.knightlab.com/libs/timeline3/latest/js/timeline.js"          
+          onLoad={this.displayTimeline.bind(this)}
         />
-        {/* This doesn't work */}
-        {/* <script src="https://cdn.knightlab.com/libs/timeline3/latest/js/timeline.js" /> */}
-      </Helmet>
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h1 className="blog-title">{title}</h1>
-              <p>{description}</p>
-              <PostContent content={content} />
-              {tags && tags.length ? (
-                <div style={{ marginTop: `4rem` }}>
-                  <h4>Tags</h4>
-                  <ul className="taglist">
-                    {tags.map(tag => (
-                      <li key={tag + `tag`}>
-                        <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ) : null}
+        <div className="container content">
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+              <div className="section">
+                <h1 className="blog-title">{title}</h1>
+                <p>{description}</p>
+
+                <PostContent content={content} />
+                <div id="timelineID" style={{ height: "100%", width: "100%" }} />
+                {tags && tags.length ? (
+                  <div style={{ marginTop: `4rem` }}>
+                    <h4>Tags</h4>
+                    <ul className="taglist">
+                      {tags.map(tag => (
+                        <li key={tag + `tag`}>
+                          <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ) : null}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  }
+}
 
 BlogPostTemplate.propTypes = {
   content: PropTypes.string.isRequired,
